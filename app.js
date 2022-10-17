aa6e2868d9b4fc0c4f83ef225f667a47f0ac5447
Genaro Zottele <genarozottele14@gmail.com>
2022-10-17 17:14:15 -0300

    express

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 const express = require('express');
+const res = require('express/lib/response');
+const app = express();
+
+const path = require('path');
+const publicFolderPath = path.resolve('public');
+app.use(express.static(publicFolderPath));
+
+const APP_PORT = 1234;
+app.listen(APP_PORT, () => {
+    console.log('Servidor funcionando en puerto ' + APP_PORT)
+});
+
+app.get('/',(req, res)=>{
+    res.sendFile(path.resolve('./views/home.html'))
+});
+
+app.get('/register.html',(req, res)=>{
+    res.sendFile(path.resolve('./views/register.html'))
+})
